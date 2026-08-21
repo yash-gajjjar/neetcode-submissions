2f15ebfcd0a0e48daf8ee779301899a3624924c1
Yash Gajjar <99782060+yash-gajjjar@users.noreply.github.com>
2026-08-21 19:22:07 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            return '🫥'
+        return '😀'.join(strs)
+    def decode(self, s: str) -> List[str]:
+        if s == '🫥':
+            return []
+        return s.split('😀')
+
+
+# Time - O(N), Space - O(N)
